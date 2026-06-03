729ea5c149ee3a590258be83f4e44656fa3e255c
Jeys Almone <jeys532667@gmail.com>
2026-06-03 11:14:05 -0300

    implementa tradução das operações gt e lt

--- code_writer.py
+++ code_writer.py
@@ -6,6 +6,34 @@ class CodeWriter:
     def write_line(self, line):
         self.file.write(line + "\n")
 
+    def write_comparison(self, jump_command):
+        true_label = f"TRUE_{self.label_count}"
+        end_label = f"END_{self.label_count}"
+        self.label_count += 1
+
+        self.write_line("@SP")
+        self.write_line("AM=M-1")
+        self.write_line("D=M")
+        self.write_line("A=A-1")
+        self.write_line("D=M-D")
+
+        self.write_line(f"@{true_label}")
+        self.write_line(f"D;{jump_command}")
+
+        self.write_line("@SP")
+        self.write_line("A=M-1")
+        self.write_line("M=0")
+
+        self.write_line(f"@{end_label}")
+        self.write_line("0;JMP")
+
+        self.write_line(f"({true_label})")
+        self.write_line("@SP")
+        self.write_line("A=M-1")
+        self.write_line("M=-1")
+
+        self.write_line(f"({end_label})")
+
     def write_arithmetic(self, command):
 
         operations = {
@@ -33,32 +61,13 @@ class CodeWriter:
             self.write_line("M=!M")
 
         elif command == "eq":
-            true_label = f"TRUE_{self.label_count}"
-            end_label = f"END_{self.label_count}"
-            self.label_count += 1
+            self.write_comparison("JEQ")
 
-            self.write_line("@SP")
-            self.write_line("AM=M-1")
-            self.write_line("D=M")
-            self.write_line("A=A-1")
-            self.write_line("D=M-D")
+        elif command == "gt":
+            self.write_comparison("JGT")
 
-            self.write_line(f"@{true_label}")
-            self.write_line("D;JEQ")
-
-            self.write_line("@SP")
-            self.write_line("A=M-1")
-            self.write_line("M=0")
-
-            self.write_line(f"@{end_label}")
-            self.write_line("0;JMP")
-
-            self.write_line(f"({true_label})")
-            self.write_line("@SP")
-            self.write_line("A=M-1")
-            self.write_line("M=-1")
-
-            self.write_line(f"({end_label})")
+        elif command == "lt":
+            self.write_comparison("JLT")
 
     def close(self):
         self.file.close()
