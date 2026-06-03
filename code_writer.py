9b1428d8d4b4d378ab3eb6eab4daaff770acc7c3
Jeys Almone <jeys532667@gmail.com>
2026-06-03 11:11:42 -0300

    implementa tradução da operação eq

--- code_writer.py
+++ code_writer.py
@@ -1,6 +1,7 @@
 class CodeWriter:
     def __init__(self, filename):
         self.file = open(filename, "w")
+        self.label_count = 0
 
     def write_line(self, line):
         self.file.write(line + "\n")
@@ -30,6 +31,34 @@ class CodeWriter:
             self.write_line("@SP")
             self.write_line("A=M-1")
             self.write_line("M=!M")
+
+        elif command == "eq":
+            true_label = f"TRUE_{self.label_count}"
+            end_label = f"END_{self.label_count}"
+            self.label_count += 1
+
+            self.write_line("@SP")
+            self.write_line("AM=M-1")
+            self.write_line("D=M")
+            self.write_line("A=A-1")
+            self.write_line("D=M-D")
+
+            self.write_line(f"@{true_label}")
+            self.write_line("D;JEQ")
+
+            self.write_line("@SP")
+            self.write_line("A=M-1")
+            self.write_line("M=0")
+
+            self.write_line(f"@{end_label}")
+            self.write_line("0;JMP")
+
+            self.write_line(f"({true_label})")
+            self.write_line("@SP")
+            self.write_line("A=M-1")
+            self.write_line("M=-1")
+
+            self.write_line(f"({end_label})")
 
     def close(self):
         self.file.close()
