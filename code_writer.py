f63e9e1b7434c02c84bc6c692ad6a9ad84aaf30d
Jeys Almone <jeys532667@gmail.com>
2026-06-03 10:58:52 -0300

    implementa tradução da operação add

--- code_writer.py
+++ code_writer.py
@@ -5,5 +5,21 @@ class CodeWriter:
     def write_line(self, line):
         self.file.write(line + "\n")
 
+    def write_arithmetic(self, command):
+
+        operations = {
+            "add": "M=D+M",
+            "sub": "M=M-D",
+            "and": "M=D&M",
+            "or": "M=D|M"
+        }
+
+        if command in operations:
+            self.write_line("@SP")
+            self.write_line("AM=M-1")
+            self.write_line("D=M")
+            self.write_line("A=A-1")
+            self.write_line(operations[command])
+
     def close(self):
         self.file.close()
