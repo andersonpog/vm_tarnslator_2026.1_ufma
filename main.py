e79f5e6c0f9fdde859e5c30ebbcbee3004d4a77e
Jeys Almone <jeys532667@gmail.com>
2026-06-03 10:40:28 -0300

    adiciona identificação de tipos e argumentos no parser

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ def main():
     parser = Parser("tests/teste.vm")
 
     while parser.has_more_commands():
-        command = parser.command()
-        print(command)
+        parser.advance()
+
+        print("Comando:", parser.current_command)
+        print("Tipo:", parser.command_type())
+        print("Arg1:", parser.arg1())
+
+        if parser.command_type() in ["C_PUSH", "C_POP"]:
+            print("Arg2:", parser.arg2())
+
+        print("---")
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -1,8 +1,15 @@
 class Parser:
-    def __init__(self, filename):
-        with open(filename, "r") as file:
-            self.commands = []
+    ARITHMETIC_COMMANDS = [
+        "add", "sub", "neg",
+        "eq", "gt", "lt",
+        "and", "or", "not"
+    ]
 
+    def __init__(self, filename):
+        self.commands = []
+        self.current_command = None
+
+        with open(filename, "r") as file:
             for line in file:
                 line = line.split("//")[0].strip()
 
@@ -12,7 +19,31 @@ class Parser:
     def has_more_commands(self):
         return len(self.commands) > 0
 
-    def command(self):
+    def advance(self):
         if self.has_more_commands():
-            return self.commands.pop(0)
+            self.current_command = self.commands.pop(0)
+            return self.current_command
         return None
+
+    def command_type(self):
+        command = self.current_command[0]
+
+        if command == "push":
+            return "C_PUSH"
+
+        if command == "pop":
+            return "C_POP"
+
+        if command in self.ARITHMETIC_COMMANDS:
+            return "C_ARITHMETIC"
+
+        return None
+
+    def arg1(self):
+        if self.command_type() == "C_ARITHMETIC":
+            return self.current_command[0]
+
+        return self.current_command[1]
+
+    def arg2(self):
+        return int(self.current_command[2])
