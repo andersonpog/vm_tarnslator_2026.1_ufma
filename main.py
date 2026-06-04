904100acf377570891b41fae9d758c4677b1d6ea
andersonpog <silveira.anderson.1988@gmail.com>
2026-06-04 18:06:00 -0300

    Modificação da main para aceitar nome qualquer de arquivo na entrada.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import sys
+import os
+
 from parser import Parser
 from code_writer import CodeWriter
 
 
 def main():
-    input_file = "tests/teste.vm"
-    output_file = "tests/teste.asm"
+    if len(sys.argv) != 2:
+        print("Erro: Uso correto: python main.py <caminho_do_arquivo.vm>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    # ALERTA: Verifica se o caminho existe e se realmente aponta para um arquivo
+    if not os.path.exists(input_file) or not os.path.isfile(input_file):
+        print(f"Erro: O caminho '{input_file}' é inválido ou o arquivo não existe.")
+        sys.exit(1)
+
+    # Validação extra: Garante que o arquivo possui a extensão correta (.vm)
+    if not input_file.endswith(".vm"):
+        print("Erro: O arquivo de entrada deve ter a extensão '.vm'.")
+        sys.exit(1)
+
+    # Troca a extensão de .vm para .asm
+    output_file = input_file.replace(".vm", ".asm")
 
     parser = Parser(input_file)
     writer = CodeWriter(output_file)
